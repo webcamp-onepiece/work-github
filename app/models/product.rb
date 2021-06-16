4ebce95acb7ad053821c43ace85ae3c0751b3043
EC2 Default User <ec2-user@ip-172-31-73-207.ec2.internal>
2021-06-16 10:03:43 +0000

    Add validation,Association,controller to genre and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+  belongs_to :genre
+  has_many :cart_items
+  has_many :order_products
+
+  with_options presence: true do
+  validates :name
+  validates :description
+  validates :price
+  validates :is_active
+  end
+
 end
