5848e4ec0a328dfd67a329028b8fe51bb0eb9ea9
kento1101 <ec2-user@ip-172-31-56-36.ec2.internal>
2021-06-16 09:30:13 +0000

    ADD receiver,costomerのアソシエーション、バリデーション、コントローラ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,21 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         
+         
+  with_options presence: true do
+   validates :family_name
+   validates :first_name
+   validates :family_name_kana
+   validates :first_name_kana
+   validates :postal_code
+   validates :address
+   validates :phone_number
+   validates :email
+   validates :encrypted_password
+   validates :is_deleted
+  end
+         
+         
 end
