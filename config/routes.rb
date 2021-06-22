d9d9532461be940839329c2e2eda0a446617bbb1
usagigensui <usagigensuii@gmail.com>
2021-06-22 06:15:24 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,39 +11,41 @@ Rails.application.routes.draw do
 }
 
   #カスタマー
-root to: 'public/homes#top'
-get "about" => "public/homes#about" , as: "about"
+  scope module: :public do
 
-get "customers/my_page" => 'public/customers#show'
-get "customers/my_page/edit" => 'public/customers#edit', as: "my_page_edit"
-patch "customers" => 'public/customers#update'
+    root to: 'homes#top'
+    get "about" => "homes#about" , as: "about"
 
-
-get "customers/alert" => "public/customers#alert"
-patch "customers/withdraw" => "public/customers#withdraw"
+    get "customers/my_page" => 'customers#show'
+    get "customers/my_page/edit" => 'customers#edit', as: "my_page_edit"
+    patch "customers" => 'customers#update'
 
 
-get "customers/products" => "public/products#index"
-get "customers/product/:id" => "public/products#show", as:'customers_product'
-
-
-namespace :public do
-  resources :cart_items, only: [:index, :create, :update, :destroy] do
-    collection do
-      delete 'destroy_all'
+    get "customers/alert" => "customers#alert"
+    patch "customers/withdraw" => "customers#withdraw"
+      resources :cart_items, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete 'destroy_all'
+      end
     end
-  end
-end
 
-  #注文情報
-  resources :orders, only: [:index,:new,:create,:show], module: "public" do
-    collection do
-      post "confirm"
-      get "thankyou"
+
+    get "customers/products" => "products#index"
+    get "customers/product/:id" => "products#show", as:'customers_product'
+
+    #注文
+    resources :orders, only: [:index,:new,:create,:show] do
+      collection do
+        post "confirm"
+        get "thankyou"
+      end
     end
+
+    # 配送先
+    resources :receivers, only: [:index, :create, :edit, :update, :destroy,]
   end
 
-resources :receivers, only: [:index, :create, :edit, :update, :destroy,]
+
 
   # 管理者
   namespace :admin do
