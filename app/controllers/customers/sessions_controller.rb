0f592977885eb099c8d228cae60bda689a91cd95
kento1101 <teshimaaaaa1101@gmail.com>
2021-06-24 08:49:39 +0000

    エラーの修正

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Customers::SessionsController < Devise::SessionsController
+  before_action :reject_inactive_customer, only: [:create]
+  
+  
+  def reject_inactive_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+      if @customer.is_deleted == false
+        redirect_to new_customer_session_path
+      end  
+  end
+  
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
