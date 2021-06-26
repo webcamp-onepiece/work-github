8e6ebc693e80b1d73e6a11183aa1808087ed9e81
usagigensui <usagigensuii@gmail.com>
2021-06-26 04:48:23 +0000

    不具合修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,40 +1,42 @@
 class Public::CartItemsController < ApplicationController
-  
+
   before_action :authenticate_customer!
-  
+
   def index
     @cart_items = current_customer.cart_items
+    @products = Product.where(is_active: "true")
+    @new_products = @products.limit(4).order(" created_at DESC ")
   end
-  
+
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.save
     redirect_to  cart_items_path
   end
-  
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     @cart_item.save
      redirect_to  cart_items_path
   end
-  
+
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     redirect_to  cart_items_path
   end
-  
+
   def destroy_all
     current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
-  
+
   private
-  
+
   def cart_item_params
     params.require(:cart_item).permit(:customer_id, :product_id, :count)
   end
-  
+
 end
