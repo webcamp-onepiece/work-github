6e77220eb9830a3f7ee83a3f6c2361e3c766b46d
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-06-27 03:51:53 +0000

    画像表示修正、ページ修正

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,6 @@
 class Public::ProductsController < ApplicationController
   def index
-    @product = Product.page(params[:page]).reverse_order
-    @products = Product.where(is_active: "true")
+    @products = Product.all.page(params[:page]).where(is_active: "true")
   end
   
   def show
